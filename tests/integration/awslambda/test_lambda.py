dd52262bc02ff67616ed0e682fb39594a1c5c7e6
Daniel Fangl <daniel.fangl@localstack.cloud>
2023-07-05 17:37:23 +0200

    wip

--- tests/integration/awslambda/test_lambda.py
+++ tests/integration/awslambda/test_lambda.py
@@ -987,6 +987,7 @@ class TestLambdaFeatures:
         snapshot.match("invoke-result", result)
 
         assert 202 == result["StatusCode"]
+        time.sleep(10)
 
     @markers.snapshot.skip_snapshot_verify(
         condition=is_old_provider, paths=["$..LogResult", "$..ExecutedVersion"]
